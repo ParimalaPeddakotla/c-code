31a42c80db28a6215119db65722a015baa229e63
ParimalaPeddakotla <93815304+ParimalaPeddakotla@users.noreply.github.com>
2025-09-24 17:41:07 +0530

    Update hello.c

--- hello.c
+++ hello.c
@@ -3,7 +3,7 @@
 int main() {
     printf("Hello from Jenkins CI/CD Pipeline to AWS EC2!\\n");
     printf("Happy Navarathri");
-    printf("om shiva");
+    printf("om shiva shakthi");
     return 0;
 }
 
